a3e20a1cb9d1eafa50d3bc41b5b74a40595949ea
Muhammed Necati Polat <mnecatipolat@gmail.com>
2023-12-14 22:43:33 +0300

    connect to firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBFKCHCg-GsWil4dywv05qZ_XUWJk_FW20",
+  authDomain: "bastirchef-3aeef.firebaseapp.com",
+  projectId: "bastirchef-3aeef",
+  storageBucket: "bastirchef-3aeef.appspot.com",
+  messagingSenderId: "353788783151",
+  appId: "1:353788783151:web:45765e2f49dd4dac1334da"
+};
+
+// Initialize Firebase
+export const firebaseApp = initializeApp(firebaseConfig);
--- src/main.js
+++ src/main.js
@@ -2,13 +2,17 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
+import { VueFire, VueFireAuth, VueFireFirestoreOptionsAPI } from 'vuefire'
 import App from './App.vue'
 import router from './router'
+import { firebaseApp } from '@/firebase'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.use(VueFire, {
+  firebaseApp,
+  modules: [VueFireAuth, VueFireFirestoreOptionsAPI]
+})
 app.mount('#app')
