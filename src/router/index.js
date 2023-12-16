6b8954a5f582b8bb09cb5cf3eced58f567fd5ea5
Muhammed Necati Polat <mnecatipolat@gmail.com>
2023-12-16 13:42:11 +0300

    add routes and route authentication

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,51 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import { getAuth } from 'firebase/auth'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: HomeView
+      component: () => import('../views/HomeView.vue'),
+      meta: { requiresAuth: true }
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/login',
+      name: 'login',
+      component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue')
+    },
+    {
+      path: '/logout',
+      name: 'logout',
+      component: () => import('../views/LogoutView.vue')
     }
   ]
+})
+
+router.beforeEach(async (to) => {
+  // routes with `meta: { requiresAuth: true }` will check for
+  // the users, others won't
+  if (to.meta.requiresAuth) {
+    const currentUser = getAuth().currentUser
+    console.log(getAuth().currentUser)
+    // if the user is not logged in, redirect to the login page
+    if (!currentUser) {
+      return {
+        path: '/login',
+        query: {
+          // we keep the current path in the query so we can
+          // redirect to it after login with
+          // `router.push(route.query.redirect || '/')`
+          redirect: to.fullPath
+        }
+      }
+    }
+  }
 })
 
 export default router
