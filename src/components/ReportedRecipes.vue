0a7ab4c0b2f22fd5223654d455ecc94f825592e4
Muhammed Necati Polat <mnecatipolat@gmail.com>
2024-01-01 16:15:55 +0300

    change reported recipy to adapt new db structure

---
+++ src/components/ReportedRecipes.vue
@@ -0,0 +1,68 @@
+<script setup>
+import { collection, getDocs, query, where, doc, deleteDoc, updateDoc } from 'firebase/firestore'
+import { useFirestore } from 'vuefire'
+import { ref } from 'vue'
+const recipes = ref([])
+const db = useFirestore()
+const recipesCollection = collection(db, 'recipes')
+const q = query(recipesCollection, where('isReported', '==', true))
+
+const refreshRecipes = async () => {
+  const querySnap = await getDocs(q)
+  recipes.value = []
+  for (const doc of querySnap.docs) {
+    const docData = doc.data()
+    recipes.value.push({
+      recipeId: doc.id,
+      recipeName: docData.recipeName,
+      recipeText: docData.recipeText,
+      timeCreated: docData.timeCreated,
+      userId: docData.userId
+    })
+  }
+}
+
+refreshRecipes()
+
+const deleteRecipe = async (id) => {
+  await deleteDoc(doc(recipesCollection, id))
+}
+
+const removeReportFlag = async (id) => {
+  if (confirm('Are you sure you want to remove the report flag?')) {
+    await updateDoc(doc(recipesCollection, id), {
+      isReported: false
+    })
+    refreshRecipes()
+  }
+}
+</script>
+
+<template>
+  <table>
+    <tr>
+      <th>Recipe Name</th>
+      <th>Recipe Text</th>
+      <th>Time Created</th>
+      <th>User Id</th>
+      <th>Actions</th>
+    </tr>
+    <tr v-for="recipe in recipes" :key="recipe.id">
+      <td>{{ recipe.recipeName }}</td>
+      <td>{{ recipe.recipeText }}</td>
+      <td>{{ recipe.timeCreated }}</td>
+      <td>{{ recipe.userId }}</td>
+      <td>
+        <button @click="deleteRecipe(recipe.recipeId)">Delete</button>
+        <button @click="removeReportFlag(recipe.recipeId)">Remove</button>
+      </td>
+    </tr>
+    <tr>
+      <th>Recipe Name</th>
+      <th>Recipe Text</th>
+      <th>Time Created</th>
+      <th>User Id</th>
+      <th>Actions</th>
+    </tr>
+  </table>
+</template>
--- src/views/ReportedRecipesView.vue
+++ src/views/ReportedRecipesView.vue
@@ -1,92 +1,11 @@
 <script setup>
-import { useCollection, useFirestore } from 'vuefire'
-import { collection, doc, getDoc } from 'firebase/firestore'
-import router from '../router'
-import { ref, onMounted, onUnmounted, watchEffect } from 'vue';
-import { useStore } from 'vuex';
-
-const store = useStore();
-const db = useFirestore()
-const reportedRecipes = useCollection(collection(db, 'reported-recipes'))
-const recipesWithNames = ref([]);
-
-const getRecipeName = async (id) => {
-  try {
-    const recipesCollection = collection(db, 'recipes');
-    const recipeDoc = doc(recipesCollection, id);
-
-    const docSnapshot = await getDoc(recipeDoc);
-
-    if (docSnapshot.exists()) {
-      return docSnapshot.data().recipeName;
-    } else {
-      console.warn('Recipe not found for ID:', id);
-      return null;
-    }
-  } catch (error) {
-    console.error('Error fetching recipe:', error);
-    return null;
-  }
-}
-
-const deleteRecipe = (id) => {
-  router.push({name: 'recipes/delete', params: {id: id}})
-}
-
-const removeSuggestion = (id) => {
-  router.push({name: 'reportedRecipes/remove', params: {id: id}})
-}
-
-const refreshData = async () => {
-  recipesWithNames.value = [];
-  await Promise.all(reportedRecipes.value.map(async (recipe) => {
-    const recipeName = await getRecipeName(recipe.recipeId);
-    if (recipeName !== null) {
-      recipesWithNames.value.push({ ...recipe, recipeName });
-    }
-  }));
-  store.commit('setDataLoaded', true);
-  console.log("set but true")
-}
-
-watchEffect(() => {
-    store.state.dataLoaded = false;
-})
-
-onMounted(async () => {
-    await new Promise(resolve => setTimeout(resolve, 100));
-    console.log("hello there")
-  if (!store.state.dataLoaded) {
-    refreshData();
-    console.log("mmm refress")
-  }
-});
-
-onUnmounted(() => {
-    store.commit('setDataLoaded', false);
-});
+import ReportedRecipes from '@/components/ReportedRecipes.vue'
 </script>
-
 <template>
-    <table>
-    <tr>
-      <th>Recipe Name</th>
-      <th>Actions</th>
-    </tr>
-    <tr v-for="recipe in recipesWithNames" :key="recipe.id">
-      <td>{{ recipe.recipeName }}</td>
-      <td>
-        <button @click="deleteRecipe(recipe.recipeId)">
-            Delete
-        </button>
-        <button @click="removeSuggestion(recipe.recipeId)">
-            Remove
-        </button>
-      </td>
-    </tr>
-    <tr>
-      <th>Recipe Name</th>
-      <th>Actions</th>
-    </tr>
-  </table>
+    <Suspense>
+        <template #fallback>
+            <div>Loading...</div>
+        </template>
+        <ReportedRecipes />
+    </Suspense>
 </template>
