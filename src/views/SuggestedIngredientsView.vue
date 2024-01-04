66f19ecefcabac3abb36cfeb0fbfa9daea0e3e21
Muhammed Necati Polat <mnecatipolat@gmail.com>
2024-01-04 17:31:37 +0300

    get unverified ingredients

--- src/views/SuggestedIngredientsView.vue
+++ src/views/SuggestedIngredientsView.vue
@@ -1,75 +1,72 @@
 <script setup>
 import { useCollection, useFirestore } from 'vuefire'
-import { collection, doc, addDoc, deleteDoc, updateDoc } from 'firebase/firestore'
-import EditModal from '@/components/EditModal.vue';
-import { ref } from 'vue';
+import { collection, doc, deleteDoc, updateDoc, query, where } from 'firebase/firestore'
+import EditModal from '../components/EditModal.vue'
+import {ref} from 'vue'
 const db = useFirestore()
-const suggestedIngredientsCollection = collection(db, 'suggested-ingredients')
-const ingredients = useCollection(suggestedIngredientsCollection)
-
+const ingredientsCollection = collection(db, 'ingredients')
+const q = query(ingredientsCollection, where('isVerified', '!=', true))
+const ingredients = useCollection(q)
 const open = ref(false)
 const currentlyEditingIngredient = ref(null)
-const editSuggestedIngredient = (choosenIngredient) => {
-  currentlyEditingIngredient.value = choosenIngredient
-  open.value = true
-} 
-
-const saveAction = async (ingredient) => {
-  await updateDoc(doc(suggestedIngredientsCollection, ingredient.id), {
-    name: ingredient.name,
-    unit: ingredient.unit
-  })
-  open.value = false
+const editIngredient = (choosenIngredient) => {
+    currentlyEditingIngredient.value = choosenIngredient
+    open.value = true
 }
-
-const removeSuggestedIngredient = (id) => {
-  if (confirm('Are you sure you want to remove this suggestion?')) {
-    const ingredientDoc = doc(suggestedIngredientsCollection, id)
-    deleteDoc(ingredientDoc)
-  }
-}
-const addSuggestedIngredient = async (name, unit, id) => {
-  if (confirm('Are you sure you want to add this ingredient?')) {
-    const ingredientsCollection = collection(db, 'ingredients')
-    await addDoc(ingredientsCollection, { name: name, unit: unit })
-    const ingredientDoc = doc(suggestedIngredientsCollection, id)
-    await deleteDoc(ingredientDoc)
-  }
+const deleteIngredient = async (id) => {
+    if(confirm('Are you sure you want to delete this ingredient?')){
+       await deleteDoc(doc(ingredientsCollection, id))
+    }
 }
 
 const closeAction = () => {
-  open.value = false
+    open.value = false
 }
 
+const saveAction = async (ingredient) => {
+    await updateDoc(doc(ingredientsCollection, ingredient.id), {
+        name: ingredient.name,
+        unit: ingredient.unit
+    })
+    open.value = false
+}
+
+const verifyIngredient = async (id) => {
+    if(confirm('Are you sure you want to verify this ingredient?')){
+        await updateDoc(doc(ingredientsCollection, id), {
+            isVerified: true
+        })
+    }
+}
 </script>
 <template>
-  <EditModal
-    v-if="open"
-    :data="currentlyEditingIngredient"
-    @close="closeAction"
-    @save="saveAction(currentlyEditingIngredient)"
-  />
-  <table>
-    <tr>
-      <th>Ingredient Name</th>
-      <th>Unit</th>
-      <th>Actions</th>
-    </tr>
-    <tr v-for="ingredient in ingredients" :key="ingredient.id">
-      <td>{{ ingredient.name }}</td>
-      <td>{{ ingredient.unit }}</td>
-      <td>
-        <button @click="addSuggestedIngredient(ingredient.name, ingredient.unit, ingredient.id)">
-          Add
-        </button>
-        <button @click="editSuggestedIngredient(ingredient)">Edit</button>
-        <button @click="removeSuggestedIngredient(ingredient.id)">Remove</button>
-      </td>
-    </tr>
-    <tr>
-      <th>Ingredient Name</th>
-      <th>Unit</th>
-      <th>Actions</th>
-    </tr>
-  </table>
+    <EditModal v-if="open" :data="currentlyEditingIngredient" @close="closeAction" @save="saveAction(currentlyEditingIngredient)"/>
+    <table>
+        <tr>
+            <th>Ingredient Name</th>
+            <th>Unit</th>
+            <th>Is Verified?</th>
+            <th>Actions</th>
+        </tr>
+        <tr v-for="ingredient in ingredients" :key="ingredient.id">
+            <td>{{ ingredient.name }}</td>
+            <td>{{ ingredient.unit }}</td>
+            <td>{{ !(ingredient.isVerified === false) }}</td>
+            <td>
+                <button @click="editIngredient(ingredient)">
+                    Edit
+                </button>
+                <button @click="deleteIngredient(ingredient.id)">
+                    Delete
+                </button>
+                <button v-if="(ingredient.isVerified === false)" @click="verifyIngredient(ingredient.id)">Verify</button>
+            </td>
+        </tr>
+        <tr>
+            <th>Ingredient Name</th>
+            <th>Unit</th>
+            <th>Is Verified?</th>
+            <th>Actions</th>
+        </tr>
+    </table>
 </template>
